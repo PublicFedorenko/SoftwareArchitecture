22393555b6e8ea4fb20b9eab1514ba68e5bd4a5c
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-03-17 12:50:11 +0300

    feat(Tamagotchi): event args for eating

---
+++ Lab3/Tamagotchi/Animal.Needs/Eating/EatingEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi.Animal.Needs.Eating
+{
+    public class EatingEventArgs : EventArgs
+    {
+        public int TimesEaten { get; private set; }
+        public bool IsStarving { get; private set; }
+
+        public EatingEventArgs(int timesEaten, bool isStarving)
+        {
+            TimesEaten = timesEaten;
+            IsStarving = isStarving;
+        }
+    }
+}
