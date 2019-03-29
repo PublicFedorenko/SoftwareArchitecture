0d2e1f22bd0cbb7a644f9803160b217246a31a4c
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-03-29 03:55:41 +0300

    fix(Tamagotchi): namespace reference fixed

--- Lab3/Tamagotchi/Owners/IOwner.cs
+++ Lab3/Tamagotchi/Owners/IOwner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tamagotchi.Animals;
+using Tamagotchi.Animals.Interfaces;
 
 namespace Tamagotchi.Owners
 {
