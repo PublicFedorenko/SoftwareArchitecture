6f8e0b97d8a68d730b69c85bb1f95a011263c8cf
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-03-29 03:53:57 +0300

    feat(Tamagotchi): added event StartEating

--- Lab3/Tamagotchi/Animal.Needs/Eating/IEatingNeed.cs
+++ Lab3/Tamagotchi/Animal.Needs/Eating/IEatingNeed.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tamagotchi.Animals.Cats;
 
 namespace Tamagotchi.Animal.Needs.Eating
 {
     public interface IEatingNeed
     {
         event EventHandler<EatingEventArgs> GotHungry;
+        event EventHandler<EatingEventArgs> StartEating;
 
         int TimesEaten { get; set; }
         bool IsHungry { get; set; }
