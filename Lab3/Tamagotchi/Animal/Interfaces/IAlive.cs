7fcb5c6b9a6ccd83ebb679023964503ed84a0eb9
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-03-29 04:26:06 +0300

    feat(Tamagotchi): added interface for living creatures

---
+++ Lab3/Tamagotchi/Animal/Interfaces/IAlive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi.Animal.Interfaces
+{
+    public interface IAlive
+    {
+        // there could be Health property
+        bool IsAlive { get; set; }
+    }
+}
