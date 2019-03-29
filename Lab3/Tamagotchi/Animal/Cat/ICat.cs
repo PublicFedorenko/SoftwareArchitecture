975ae15811977ac6561f05a2d84f8268903ded9d
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-03-29 04:27:20 +0300

    feat(Tamagotchi): added interface for cats

---
+++ Lab3/Tamagotchi/Animal/Cat/ICat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tamagotchi.Animal.Interfaces;
+using Tamagotchi.Animal.Need.Eating;
+
+namespace Tamagotchi.Animal.Cat
+{
+    public interface ICat : IAnimal
+    {
+        IEatingNeed EatingNeed { get; set; }
+    }
+}
