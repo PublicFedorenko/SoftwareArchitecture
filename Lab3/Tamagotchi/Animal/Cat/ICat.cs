84359baf1620af7efdad0ac59d9081dda0b6efa4
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-04-01 21:38:47 +0300

    change(Tamagotchi): injected new behaviors for cat

--- Lab3/Tamagotchi/Animal/Cat/ICat.cs
+++ Lab3/Tamagotchi/Animal/Cat/ICat.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tamagotchi.Animal.Behavior;
+using Tamagotchi.Animal.Cat.States;
 using Tamagotchi.Animal.Interfaces;
 using Tamagotchi.Animal.Need.Eating;
 
@@ -11,5 +13,8 @@ namespace Tamagotchi.Animal.Cat
     public interface ICat : IAnimal
     {
         IEatingNeed EatingNeed { get; set; }
+        IWalkBehavior WalkBehavior { get; set; }
+        ITalkBehavior TalkBehavior { get; set; }
+        ICatState State { get; set; }
     }
 }
