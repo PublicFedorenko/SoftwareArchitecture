aea4896b9f898e895bbd74fe479979de0220455a
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-04-01 21:37:46 +0300

    feat(Tamagotchi): added new behaviors

---
+++ Lab3/Tamagotchi/Animal/Behavior/IRunBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi.Animal.Behavior
+{
+    public interface IRunBehavior
+    {
+        void StartRunning();
+        void StopRunning();
+    }
+}
