9ca0ead026bb78235818ed5cb3ce2873a585d597
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-04-01 21:41:32 +0300

    feat(Tamagotchi): added new states for cat

---
+++ Lab3/Tamagotchi/Animal/Cat/States/ICatState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi.Animal.Cat.States
+{
+    public interface ICatState
+    {
+        ICat Context { get; set; }
+        void Eat();
+        void StartWalking();
+        void StopWalking();
+        void StartTalking();
+        void StopTalking();
+        void StartRunning();
+        void StopRunning();
+    }
+}
---
+++ Lab3/Tamagotchi/Animal/Cat/States/Running.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi.Animal.Cat.States
+{
+    public class Running : ICatState
+    {
+        public ICat Context { get; set; }
+
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartRunning()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTalking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartWalking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopRunning()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTalking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopWalking()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
