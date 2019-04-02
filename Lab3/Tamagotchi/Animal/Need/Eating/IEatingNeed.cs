f5137f94ae5178e307941c76461f75eb163271c4
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-04-02 20:43:29 +0300

    change(Tamagotchi): removed events from interface

--- Lab3/Tamagotchi/Animal/Need/Eating/IEatingNeed.cs
+++ Lab3/Tamagotchi/Animal/Need/Eating/IEatingNeed.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tamagotchi.Animal.Cat;
 
 namespace Tamagotchi.Animal.Need.Eating
 {
     public interface IEatingNeed
     {
-        event EventHandler<EatingEventArgs> GotHungry;
-        event EventHandler<EatingEventArgs> StartEating;
-
         int TimesEaten { get; set; }
         bool IsHungry { get; set; }
         DateTime EatingInterval { get; set; }
