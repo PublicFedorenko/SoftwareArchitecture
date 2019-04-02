e8f3f74d9bafa649a1ae50d32b8d7c5fa9caba56
PublicFedorenko <bohdan.fedorenko777@gmail.com>
2019-04-02 20:41:13 +0300

    feat(Tamagotchi): added body parts and their components

---
+++ Lab3/Tamagotchi/Animal/BodyParts/Legs/CatLeg.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi.Animal.BodyParts.Legs
+{
+    public class CatLeg : IBodyPart
+    {
+        public PartSize Size { get; set; }
+    }
+}
